4b052961c2dcd57af649b343c25b546584197f7a
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-10-06 15:16:39 +0200

    Preapre take an exercise

--- site/View/View_TakeExercise.php
+++ site/View/View_TakeExercise.php
@@ -1,3 +1,27 @@
 <?php
- echo("Hello ! Here we are into the View_TakeExercise.php :3");
+/**
+ * Created by PhpStorm.
+ * User: Cyril Goldenschue
+ * Date: 01/09/2020
+ */
+
+echo("Hello ! Here we are into the View_TakeExercise.php :3");
+
 ?>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>Take An Exercise</title>
+        <Link href="../Assets/css/styleNew.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <header class="heading creating">
+        <section class="take-exercises-container">
+            <a href="../index.php">
+                <img class="miniLogo" src="../Assets/img/logo.png">
+            </a>
+            <span class="BannerTitle">Take an exercise</span>
+        </section>
+    </header>
+</html>
